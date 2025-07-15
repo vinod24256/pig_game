bc32ca6949338fc8450179c90c316bd9fb8ca50a
vinod24256 <184947573+vinod24256@users.noreply.github.com>
2025-07-15 13:46:19 +0530

    Implement dice game logic and UI updates
    Added JavaScript logic for rolling dice, holding scores, switching players, and resetting the game. Updated initial scores in index.html and added a .hidden class in style.css to control dice visibility. These changes enable interactive gameplay between two players.

--- script.js
+++ script.js
@@ -1,1 +1,92 @@
 'use strict';
+
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+
+const diceEl = document.querySelector('.dice');
+
+const btnRoll = document.querySelector('.btn--roll');
+const btnNew = document.querySelector('.btn--new');
+const btnHold = document.querySelector('.btn--hold');
+
+const curr0El = document.getElementById('current--0');
+const curr1El = document.getElementById('current--1');
+
+let scores = [];
+let currentScore;
+let activePlayer;
+let playing;
+
+const init = function () {
+  diceEl.classList.add('hidden');
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  document
+    .querySelector(`.player--${activePlayer}`)
+    .classList.remove('player--winner');
+  document.querySelector(`.player--0`).classList.add('player--active');
+  player1El.classList.remove('player--active');
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  curr0El.textContent = 0;
+  curr1El.textContent = 0;
+};
+
+init();
+
+const switchPlayer = function () {
+  currentScore = 0;
+  document.getElementById(`current--${activePlayer}`).textContent =
+    currentScore;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
+btnRoll.addEventListener('click', function () {
+  if (playing) {
+    const dice = Math.trunc(Math.random() * 6 + 1);
+    diceEl.classList.remove('hidden');
+    console.log(dice);
+    diceEl.src = `dice-${dice}.png`;
+
+    if (dice !== 1) {
+      //add dice to current score
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      switchPlayer();
+    }
+  }
+});
+
+btnHold.addEventListener('click', function () {
+  if (playing) {
+    scores[activePlayer] += currentScore;
+    score0El.textContent = scores[0];
+    score1El.textContent = scores[1];
+
+    if (scores[activePlayer] >= 100) {
+      playing = false;
+      diceEl.classList.add('hidden');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+    } else {
+      switchPlayer();
+    }
+  }
+});
+
+btnNew.addEventListener('click', function () {
+  init();
+});
